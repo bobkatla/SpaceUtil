20d215cc7a31791e987039046663dc89f043c697
bobkatla <bertram.99.works@gmail.com>
2022-02-12 15:56:49 +1100

    CHANGE: add simple token expire check and refactoring

--- tests/get_data.py
+++ tests/get_data.py
@@ -6,40 +6,41 @@ from parameters_connect import url_auth, url_query, api_key, api_secret
 
 # make the request to get the token
 def get_token_raw(url, key, secret):
-    # maybe have to add header?
+    # header set up
     headers = CaseInsensitiveDict()
     headers["Accept"] = "application/json"
     headers["Content-Type"] = "application/json"
+
     req = {"ApiKey": key, "ApiSecret": secret}
-    res = requests.post(url, headers=headers, json=req)
-    return res
+
+    return requests.post(url, headers=headers, json=req)
 
 def get_data_raw(url, token, query):
-    # will check the header again
+    # header set up
     headers = CaseInsensitiveDict()
     headers["Accept"] = "application/json"
     headers["Content-Type"] = "application/json"
     headers["Authorization"] = f"Bearer {token}"
 
-    res = requests.post(url, headers=headers, json={"query": query})
-    return res
+    return requests.post(url, headers=headers, json={"query": query})
 
 def query_full_data(query):
     raw_token = get_token_raw(url_auth, api_key, api_secret)
     token = raw_token.json()['idToken']
-    raw_data = get_data_raw(url_query, token, query)
-    data = raw_data.json()
+    if int(raw_token.json()['expiresIn']) < 100:
+        # token is about to expire warning
+        print("The token will expire soon, please check!")
 
-    return data
+    raw_data = get_data_raw(url_query, token, query)
+    return raw_data.json()
 
 def query_data(query):
     data_full = query_full_data(query)
+
     err = data_full["errors"]
     data = data_full["data"]
-    if err:
-        return ["ERROR", err]
-    else:
-        return data
+
+    return ["ERROR", err] if err else data
 
 if __name__ == '__main__':
     # Can put the testing here
@@ -62,5 +63,5 @@ if __name__ == '__main__':
     }'''
 
     q_err = "{floorSpaces {ho he}}"
-    data = query_data(q_err)
+    data = query_data(q)
     print(data)
